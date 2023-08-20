940276c35091c4cde0fd5a27339c151101230db4
ST10022345 <ST10022345@vcconnect.edu.za>
2023-08-20 14:20:22 +0200

    push

---
+++ moderndesign/moderndesign/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,49 @@
+﻿using moderndesign.core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace moderndesign.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+
+        public RelayCommand HomeViewCommand { get; set; }
+
+        public RelayCommand DiscoveryViewCommand { get; set; }
+
+        public DiscoveryViewModel DiscoveryVm { get; set; }
+        public HomeViewModel homeVm { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return  _currentView; }
+            set 
+            { 
+                _currentView = value; 
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            homeVm = new HomeViewModel();
+            DiscoveryVm = new DiscoveryViewModel();
+            CurrentView = homeVm;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = homeVm;
+            });
+
+           DiscoveryViewCommand = new RelayCommand(o =>
+           {
+               CurrentView = DiscoveryVm;
+           });
+        }
+    }
+}
